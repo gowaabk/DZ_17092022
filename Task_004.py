dee2a79c6a2e4ce4803778e4ad78530d1bf22aca
Gowaa <gowaa@bk.ru>
2022-09-24 08:29:25 +0300

    rebuild task 4

--- Task_004.py
+++ Task_004.py
@@ -1,26 +1,41 @@
 """ Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. """
 
 
-""" def rle_fun(data):
-    res = []
-    r = 0
-    for d in data:
-        if d != r:
-            res.append(d)
-            res.append(str(1))
-            print(res)
-            r = d
+def code_string(text_str: str):
+    count = 1
+    res = ''
+    for i in range(len(text_str)-1):
+        if text_str[i] == text_str[i+1]:
+            count += 1
         else:
-            res[-1] = str(int(res[-1]) + 1)
-            print(res)
-    return res  
-       """
-my_str='aaabbbbcccccdddddd'                                
-#print("".join(rle_fun(my_str)))
-for i in my_str:
-    if i == 'b':
-        print (i)
+            res += str(count) + text_str[i]
+            count = 1
+    if count > 1 or (text_str[len(text_str)-2] != text_str[-1]):
+        res += str(count) + text_str[-1]
+    return res
 
 
-#my_str2=list(my_str)
-#print (my_str2)
+def decode_string(text_str: str):
+    number = ''
+    res = ''
+    for i in range(len(text_str)):
+        if not text_str[i].isalpha():
+            number += text_str[i]
+        else:
+            res += text_str[i] * int(number)
+            number = ''
+    return res
+
+
+my_str = ''
+with open('origin.txt', 'r') as f:
+    my_str = f.read()
+
+with open('code.txt', 'w') as f:
+    f.write(code_string(my_str))
+
+with open('code.txt', 'r') as f:
+    my_str2 = f.read()
+
+with open('decode.txt', 'w') as f:
+    f.write(decode_string(my_str2))
